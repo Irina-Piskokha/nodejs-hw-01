c03aadfb63f0a0d43a26f79d4b470ba9d8941ca4
Irina_Piskokha <4526937@gmail.com>
2025-01-14 16:41:45 +0200

    add first

---
+++ src/scripts/removeAllContacts.js
@@ -0,0 +1,15 @@
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    contacts.splice(0, contacts.length);
+
+    await writeContacts(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+removeAllContacts();
